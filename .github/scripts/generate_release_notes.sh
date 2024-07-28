534d22132c2ee52180a945a6aef031c942175044
Ivan Kovtun <jeanlacroix@ukr.net>
2024-07-28 19:51:55 +0200

    Update release note script

--- .github/scripts/generate_release_notes.sh
+++ .github/scripts/generate_release_notes.sh
@@ -1,24 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
 
 set -e
 
-# git fetch --tags
-
-LATEST_TAG=$(git describe --tags --abbrev=0 || echo "v0.0.0")
-
-if [ "$LATEST_TAG" != "v0.0.0" ]; then
-    COMMITS=$(git log ${LATEST_TAG}..HEAD --pretty=format:"%h %s" --abbrev-commit)
-else
-    COMMITS=$(git log --pretty=format:"%h %s" --abbrev-commit)
+if [ ! -d .git ]; then
+    echo "This script must be run in a Git repository."
+    exit 1
 fi
+
+# git fetch --tags # debug purposed disabled
+
+LATEST_TAG=$(git describe --tags --abbrev=0)
+
+if [ -z "$LATEST_TAG" ]; then
+    echo "No tags found in the repository."
+    exit 0
+fi
+
+echo "Latest Tag: $LATEST_TAG"
+
+COMMITS=$(git log "$LATEST_TAG"..HEAD --oneline)
 
 if [ -z "$COMMITS" ]; then
-    echo "## Release Notes" >RELEASE_NOTES.md
-    echo "No changes since the last release." >>RELEASE_NOTES.md
-else
-    echo "## Release Notes" >RELEASE_NOTES.md
-    echo "$COMMITS" >>RELEASE_NOTES.md
+    echo "No new commits since the latest tag."
+    exit 0
 fi
+
+RELEASE_NOTES="### Release Notes since $LATEST_TAG:\n\n"
+RELEASE_NOTES+="$(echo "$COMMITS" | sed 's/^/- /')"
+
+echo -e "$RELEASE_NOTES" >RELEASE_NOTES.md
+
+echo -e "$RELEASE_NOTES"
+
+echo "Release notes generated successfully."
 
 echo "::set-output name=notes::$(cat RELEASE_NOTES.md)"
 
